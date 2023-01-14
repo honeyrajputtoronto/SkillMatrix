92826194b5d4c7d728d0aff2baff5a85a451ac99
Honey <er.honeyraj2016@gmail.com>
2023-01-14 03:07:12 -0800

    first commit

---
+++ hustlers/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class RegisterHustler(models.Model):
+    name = models.CharField(max_length=255)
+    university = models.TextField()
+    skills = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
+
+
+class RegisterRecruiter(models.Model):
+    name = models.CharField(max_length=255)
+    company_name = models.TextField()
+    skills = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
