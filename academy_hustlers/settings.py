4c7c2b3126ca94ed6e2d038c025ba0cee566ef0d
Honey <er.honeyraj2016@gmail.com>
2023-01-14 03:27:20 -0800

    new

--- academy_hustlers/settings.py
+++ academy_hustlers/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m$brugj%nb&mlrm@e$ga-8m#ccx89=ejwcopn4=3nvt&%^(a0^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
