04d0d5ff1c5da89ae2f8cb1b51c7f2e0fd850522
Amrit5s2002 <guramrit1066@gmail.com>
2023-07-28 04:03:49 +0530

    static added

--- academy_hustlers/settings.py
+++ academy_hustlers/settings.py
@@ -161,8 +161,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
