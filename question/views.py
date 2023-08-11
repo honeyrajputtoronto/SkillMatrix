fd86b4d0590d454a3ed27b5b23256da12b494bf9
BHAVYA TYAGI <72131872+Bhavya1912@users.noreply.github.com>
2023-08-11 22:28:46 +0530

    Update views.py

--- question/views.py
+++ question/views.py
@@ -49,4 +49,22 @@ class QuestionView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-
+# Truer False Question
+class T_FViewSet(viewsets.ModelViewSet):
+    queryset = T_F.objects.all()
+    serializer_class = T_FSerializer
+    
+    @action(detail=False, methods=['GET'])
+    def questions_by_level(self, request):
+        level = request.query_params.get('level')
+        questions = T_F.objects.filter(question__level=level)
+        serializer = T_FSerializer(questions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def update(self, request, pk=None):
+        instance = get_object_or_404(T_F, pk=pk)
+        serializer = T_FSerializer(instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
