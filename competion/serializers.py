fc27a72fbe27aecb2cfe7221f1f6970d7ee1713e
Amrit5s2002 <guramrit1066@gmail.com>
2023-08-28 23:25:04 +0530

    chat app

--- competion/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Competition
-
-class CompetitionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Competition
-        fields = '__all__'
-        
--- question/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import QuestionView
-
-urlpatterns = [
-    path("questions/", QuestionView.as_view(), name="questions"),
-    path('questions/<int:level>/',QuestionView.as_view(), name='question-detail'),
-]
